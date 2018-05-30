9f3b69e39a9888e9eeb082e9abdc255ba3541cd8
Kestler Andreas <kestler@wimex.rgst>
2018-05-30 16:23:09 +0200

    Projektdateien hinzufügen.

---
+++ RenamerPersonal/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace RenamerPersonal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach (var file in args)
+            {
+                if (file.Length > 0 && File.Exists(file))
+                {
+                    Console.WriteLine(file);
+                    Console.WriteLine("New name: " + Path.GetDirectoryName(file) + "\\" + DateTime.Now.ToString("yyyyMMdd") + "_" + Path.GetFileName(file));
+                    System.IO.File.Move(file, Path.GetDirectoryName(file) + "\\" + DateTime.Now.ToString("yyyyMMdd") + "_" + Path.GetFileName(file));
+                }
+            }
+            //Console.Read();
+        }
+    }
+}
